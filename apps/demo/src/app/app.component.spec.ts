9909e1c90cd861da7ebdd2729f6c5f838f76b151
Bastien <bmoulia@gmail.com>
2025-05-25 00:02:51 +0200

    refactor: update test case to skip and modify app component tests
    - Marked the 'has title' test as skipped in example.spec.ts.
    - Removed NxWelcomeComponent from AppComponent tests and adjusted imports.
    - Simplified AppComponent tests to focus on component creation.
    - Deleted nx-welcome.component.ts as it is no longer needed.
    - Updated index.html title from 'demo' to 'NGX Overlay'.
    - Updated favicon.ico file.

--- apps/demo/src/app/app.component.spec.ts
+++ apps/demo/src/app/app.component.spec.ts
@@ -1,25 +1,22 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent, NxWelcomeComponent, RouterModule.forRoot([])],
+      imports: [AppComponent, RouterModule.forRoot([])],
     }).compileComponents();
-  });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Welcome demo');
   });
 
-  it(`should have as title 'demo'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('demo');
+  it('should create', () => {
+    expect(component).toBeTruthy();
   });
 });
