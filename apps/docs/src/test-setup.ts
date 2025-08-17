424befeaa38e83d5c1f817db1b033a0cd542a574
Bastien <bmoulia@gmail.com>
2025-08-17 17:37:53 +0200

    fix: update test setup and dependencies for improved compatibility

--- apps/docs/src/test-setup.ts
+++ apps/docs/src/test-setup.ts
@@ -1,13 +1,18 @@
-import '@angular/compiler';
-import '@analogjs/vitest-angular/setup-zone';
+import '@analogjs/vitest-angular/setup-snapshots';
 
+import { provideZonelessChangeDetection, NgModule } from '@angular/core';
 import {
   BrowserTestingModule,
   platformBrowserTesting,
 } from '@angular/platform-browser/testing';
 import { getTestBed } from '@angular/core/testing';
 
+@NgModule({
+  providers: [provideZonelessChangeDetection()],
+})
+export class ZonelessTestModule {}
+
 getTestBed().initTestEnvironment(
-  BrowserTestingModule,
+  [BrowserTestingModule, ZonelessTestModule],
   platformBrowserTesting(),
 );
