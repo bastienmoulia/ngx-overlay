7cd32d6a1267906803a098988d0700ad169b5b0c
Bastien <bmoulia@gmail.com>
2025-08-17 19:50:27 +0200

    fix: ensure route is defined in GettingStartedPage configuration

--- apps/docs/src/getting-started/ng-doc.page.ts
+++ apps/docs/src/getting-started/ng-doc.page.ts
@@ -3,6 +3,7 @@ import { NgDocPage } from '@ng-doc/core';
 const GettingStartedPage: NgDocPage = {
   title: `Getting started`,
   mdFile: './index.md',
+  route: '',
 };
 
 export default GettingStartedPage;
